accd434d4d02bffc92457fbf4b38223e1bfc86db
Alex <shadowsam@mail.ru>
2021-02-04 19:01:39 +0300

    Исправлены подсчеты в 5 задаче.

--- Lesson_2_5_Slim/Program.cs
+++ Lesson_2_5_Slim/Program.cs
@@ -35,60 +35,41 @@ namespace Lesson_2_5_Slim
 		{
 			string result = "";
 			double norm;
-
-			if (index <= 18.50)
+						
+			if (index <= 16.50)
 			{
 				norm = (18.5 - index) * height * height;
-				result = string.Format($"\nИМТ {index:N2} - недостаточная масса тела\nНеобходимо набрать не менее {norm:0} кг");
+				result = string.Format($"\nИМТ {index:N2} - выраженный дефицит массы тела\nНеобходимо набрать не менее {norm:0} кг");
+			}
+			else if (index > 16.50 && index <= 18.50)
+			{
+				norm = (18.5 - index) * height * height;
+				result = string.Format($"\nИМТ {index:N2} - недостаточная масса тела (дефицит)\nНеобходимо набрать не менее {norm:0} кг");
 			}
 			else if (index > 18.50 && index <= 25.00)
 			{
 				result = string.Format($"\nИМТ {index:N2} - нормальная масса тела");
 			}
-			else if (index > 25.00)
-			{
-				norm = (index - 25) * height * height;
-				result = string.Format($"\nИМТ {index:N2} - избыточная масса тела или ожирение\nНеобходимо сбросить не менее {norm:0} кг");
-			}
-
-			#region Точный расчет (нужны правильные формулы)
-			/*
-			if (index <= 16.50)
-			{
-				norm = (16.5 - index) * height * height;  // неправильно: считает набор веса до недостаточной
-				result = string.Format($"\nИМТ {index:N2} - выраженный дефицит массы тела\nНеобходимо набрать не менее {norm:0} кг");
-			}
-			else if (index > 16.50 && index <= 18.50)
-			{
-				norm = (18.5 - index) * height * height;  // правильно
-				result = string.Format($"\nИМТ {index:N2} - недостаточная масса тела (дефицит)\nНеобходимо набрать не менее {norm:0} кг");
-			}
-			else if (index > 18.50 && index <= 25.00)  // правильно
-			{
-				result = string.Format($"\nИМТ {index:N2} - нормальная масса тела");
-			}
 			else if (index > 25.00 && index <= 30.00)
 			{
-				norm = (index - 25) * height * height;  // правильно
+				norm = (index - 25) * height * height;
 				result = string.Format($"\nИМТ {index:N2} - избыточная масса тела (предожирение)\nНеобходимо сбросить не менее {norm:0} кг");
 			}
 			else if (index > 30.00 && index <= 35.00)
 			{
-				norm = (index - 30) * height * height;  // неправильно: считает сброс веса до избыточной
+				norm = (index - 25) * height * height;
 				result = string.Format($"\nИМТ {index:N2} - ожирение I степени\nНеобходимо сбросить не менее {norm:0} кг");
 			}
 			else if (index > 35.00 && index <= 40.00)
 			{
-				norm = (index - 35) * height * height;  // неправильно: считает сброс веса до 1 степени
+				norm = (index - 25) * height * height;
 				result = string.Format($"\nИМТ {index:N2} - ожирение II степени\nНеобходимо сбросить не менее {norm:0} кг");
 			}
 			else if (index > 40.00)
 			{
-				norm = (index - 40) * height * height;  // неправильно: считает сброс веса до 2 степени
+				norm = (index - 25) * height * height;
 				result = string.Format($"\nИМТ {index:N2} - ожирение III степени\nНеобходимо сбросить не менее {norm:0} кг");
 			}
-			*/
-			#endregion
 
 			return result;
 		}
