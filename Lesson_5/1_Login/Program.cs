2ced26888ef21dfb157669f3382736075ade33ba
Alex <shadowsam@mail.ru>
2021-02-15 01:09:49 +0300

    Add Lesson 5

---
+++ Lesson_5/1_Login/Program.cs
@@ -0,0 +1,78 @@
+﻿#region Author
+// Александр Столяров
+// Домашнее задание 5.1 - Корректность логина
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Lesson_5_1_Login
+{
+	class Program
+	{
+		// проверка на латиницу
+		private static bool IsLatin(char letter)
+		{
+			int code = letter;
+			// коды стандартных символов - 65-90 = A-Z / 97-122 = a-z
+			return (code >= 65 && code <= 90) || (code >= 97 && code <= 122);
+		}
+
+		// проверка через регулярное выражение
+		static bool LoginCheckRegex(string login) => Regex.IsMatch(login, @"^[A-Za-z0-9]+${2,10}");
+
+		// проверка логина
+		private static bool LoginCheck(string login)
+		{
+			int length = login.Length;
+			if (length >= 2 && length <= 10)
+			{
+				bool check = true;
+				char letter = login[0];
+
+				if (char.IsDigit(letter))
+					return false;
+
+				for (int i = 1; i < length; i++)
+				{
+					letter = login[i];
+					if (!(char.IsDigit(letter) || IsLatin(letter)))
+					{
+						check = false;
+						break;
+					}
+				}
+
+				if (check) return true;
+			}
+			return false;
+		}
+
+		static void Main(string[] args)
+		{
+			int count = 0;
+			do
+			{
+				Console.Write("Введите логин: ");
+				string login = Console.ReadLine();
+
+				if (LoginCheck(login) && LoginCheckRegex(login))
+				{
+					Console.WriteLine("Логин корректный");
+					break;
+				}
+				else
+				{
+					Console.WriteLine("Используйте латинские буквы и цифры. Длина должна быть от 2 до 10 символов.");
+					count++;
+				}
+			} while (count < 3);
+
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Lesson_5/4_Grade/Program.cs
@@ -0,0 +1,76 @@
+﻿#region Author
+// Александр Столяров
+// Домашнее задание 5.4 - Задача ЕГЭ
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_5_4_Grade
+{
+	struct Element
+	{
+		public string name;
+		public double average;
+	}
+
+	class Program
+	{
+		// сортировка по среднему баллу
+		static void Sort(ref Element[] pupils)
+		{
+			for (int i = 0; i < pupils.Length; i++)
+			{
+				for (int j = 0; j < pupils.Length - i - 1; j++)
+				{
+					if (pupils[j].average > pupils[j + 1].average)
+					{
+						Element tmp = pupils[j + 1];
+						pupils[j + 1] = pupils[j];
+						pupils[j] = tmp;
+					}
+				}
+			}
+		}
+
+		static void Main()
+		{
+			int worstPupils = 3;
+			StreamReader stream = new StreamReader("..\\..\\grade.txt");
+			int num = int.Parse(stream.ReadLine());
+			Element[] a = new Element[num];
+
+			// читаем файл и парсим оценки
+			for (int i = 0; i < num; i++)
+			{
+				string[] s = stream.ReadLine().Split(' ');
+				a[i].name = s[0] + " " + s[1];
+				a[i].average = (double.Parse(s[2]) + double.Parse(s[3]) + double.Parse(s[4])) / 3;
+			}
+			stream.Close();  // освобождаем системные ресурсы
+
+			string result = "";
+			Sort(ref a);
+			Element prev = a[0];
+
+			for (int i = 0; i < worstPupils; i++)
+			{
+				if (i > 0)
+				{
+					if (prev.average == a[i].average)
+						worstPupils++;
+					prev = a[i];
+				}
+				result += string.Format("{0,-20} {1,-15:f2}\n", a[i].name, a[i].average);
+			}
+
+			Console.WriteLine(result);
+
+			Console.ReadKey();
+		}
+	}
+}
